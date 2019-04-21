8c55dd6b1ddb6b666aea851df011637731ec80b2
Mark Nolledo <mark.nolledo@gmail.com>
2019-04-22 02:17:53 +0800

    Remove url links to sibyl/test and sibyl/index views

--- psychopass/urls.py
+++ psychopass/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.index, name='index'),
-    path(r'test', views.testView, name='testView'),
+    #path(r'', views.index, name='index'),
+    #path(r'test', views.testView, name='testView'),
     path(r'analyze/mode=lexicon', views.analyzeLexiconMatching, name='analyzeLexiconMatching'),
     path(r'analyze/mode=classifier', views.analyzeClassifier, name='analyzeClassifier'),
     path(r'create', views.createTable, name='createTable'),
