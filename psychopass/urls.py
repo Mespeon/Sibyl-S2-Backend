3c72db6b8e5fb72e6c8013238aa10724c58d720f
Mespeon <mark.nolledo@gmail.com>
2020-02-03 18:29:34 +0800

    Added instructions in urls file; removed more unused code

--- psychopass/urls.py
+++ psychopass/urls.py
@@ -2,37 +2,31 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path(r'', views.index, name='index'),
-    #path(r'test', views.testView, name='testView'),
-    # path(r'analyze/mode=lexicon', views.analyzeLexiconMatching, name='analyzeLexiconMatching'),
-    # path(r'analyze/mode=classifier', views.analyzeClassifier, name='analyzeClassifier'),
-    # path(r'create', views.createTable, name='createTable'),
-    # path(r'write', views.writeToTable, name='writeToTable'),
-    # path(r'prepare', views.prepareData, name='prepareData'),
-    # path(r'statistics', views.prepareStatistics, name='prepareStatistics'),
-    # path(r'classify', views.aggregatedClassify, name='aggregatedClassify'),
-    # path(r'thanks', views.thanks, name='thanks'),
+    # DJANGO URL PATTERNS: An Introduction
+    # The URLs you will place here will allow you to bind specific "views"
+    # to your URLs. The views referenced here are defined in the views.py
+    # file. Whatever you do, those views should either return an HttpResponse
+    # or a JsonResponse in order to function normally.
+
+    # CREATING A URL PATTERN
     #
-    # #Ionic App API Test Functions
+    # path(r'<your_desired_url>', views.your_view_def, name='your_view_name')
+    # EXAMPLE: path(r'functions/save', views.save, name='save')
+    #
+    # WHERE:
+    # your_desired_url => any combination of URL string; it is recommended to
+    # keep this as short as possible and only add URL params if needed.
+    # NOTE: THERE IS NO NEED TO INCLUDE A FULL URL HERE; DOING SO WILL CAUSE
+    # YOUR URL PATTERN TO FAIL.
+    #
+    # your_view_def => the definition name to use as defined in the views.py file.
+    # The framework will use the view defined as the URL's template. If you have
+    # a def login(request) function, then you put it as views.login.
+    #
+    # your_view_name => the string to use as reference for the URL pattern. Only
+    # used for non-API Django usage. Preferrably to be kept the same name as the view.
+
     # path(r'ionic/testcomm', views.testComms, name='testComms'),
     path(r'ionic/stargazer', views.stargazer, name='stargazer'),
     path(r'ionic/lovelive', views.stars, name='stars'),
-    #
-    # #For Vivien
-    # path(r'vivien/hello', views.forVivien, name='forVivien'),
-    # path(r'vivien/senpai', views.forSenpai, name='forSenpai'),
-    #
-    # #For COE Toothbrush
-    # path(r'coe/like', views.coeLike, name='coeLike'),
-    # path(r'coe/react', views.coeReact, name='coeReact'),
-    #
-    # #For Sibyl API's Flutter app integrations
-    # path(r'api/create', views.createApiTable, name='createApiTable'),
-    # path(r'api/token', views.getToken, name='getToken'),
-    # path(r'api/auth', views.getAuth, name='getAuth'),
-    # path(r'api/login', views.login, name='login'),
-    # path(r'api/register', views.register, name='register'),
-    # path(r'api/logout', views.logout, name='logout'),
-    # path(r'api/listing', views.listing, name='listing'),
-    # path(r'api/authlisting', views.authlisting, name='authlisting')
 ]
